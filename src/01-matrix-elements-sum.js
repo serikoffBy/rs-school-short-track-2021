684f62819dcd7f614dad704a6acddb95a57577ea
serikoffBy <kornaush@gmail.com>
2021-04-08 23:05:37 +0300

    fix: fix task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -20,9 +20,11 @@ function getMatrixElementsSum(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (i === 0) {
-        sum += matrix[i][j]; }
+        sum += matrix[i][j];
+      }
       else if (matrix[i - 1][j] === 0) {
-        sum += 0; }
+        sum += 0;
+      }
       else {
         sum += matrix[i][j];
       }
