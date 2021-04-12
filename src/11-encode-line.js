cc7037c99d8676a5a06b850d213202658a824cae
serikoffBy <kornaush@gmail.com>
2021-04-12 13:16:23 +0300

    fix: fix function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,35 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let newStr = '';
+  let temp = str[0];
+  let count = 1;
+  if (str.length === 0) return str;
+  for(let i = 1; i < str.length; i++) {
+    if(str[i] === temp) {
+      count++;
+    }
+    else {
+      if (count === 1) {
+        newStr = newStr.concat(`${temp}`);
+        temp = str[i];
+        count = 1;
+      }
+      else {
+        newStr = newStr.concat(`${count}${temp}`);
+      }
+      temp = str[i];
+      count = 1;
+    }
+  }
+  if (count === 1) {
+    newStr = newStr.concat(str[str.length-1]);
+      }
+    else {
+      newStr = newStr.concat(`${count}${str[str.length-1]}`);
+    }
+  return newStr;
 }
 
 module.exports = encodeLine;
